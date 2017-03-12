995c4e766c5af64c5f75f893c8f2f09be39d6bf1
yang218906 <ylcj1314@gmail.com>
2017-03-13 05:31:28 +0800

    基本游戏功能实现

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Mines from './Mines';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <App />
+    <Mines />
+  </div>,
   document.getElementById('root')
 );
